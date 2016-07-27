9e8ea6b77814890133b996894e436a2c43b24de5
nicekuo <736878501@qq.com>
2016-07-27 23:20:45 +0800

    医生出诊时间页面初步

---
+++ app/src/main/java/nice/com/jzs/ui/doctors/ActivityDoctorVisitTime.java
@@ -0,0 +1,19 @@
+package nice.com.jzs.ui.doctors;
+
+import nice.com.jzs.core.AbstractActivity;
+
+/**
+ * Created by admin on 2016/7/27.
+ */
+public class ActivityDoctorVisitTime extends AbstractActivity {
+
+
+
+
+
+
+    @Override
+    protected void onClickBack() {
+
+    }
+}
