b9d411062c4d8a280b77c82c1f6ce2f07a3deb39
nicekuo <736878501@qq.com>
2016-07-31 23:01:48 +0800

    医生出诊时间完成

--- app/src/main/java/nice/com/jzs/ui/doctors/ActivityDoctorVisitTime.java
+++ app/src/main/java/nice/com/jzs/ui/doctors/ActivityDoctorVisitTime.java
@@ -1,5 +1,7 @@
 package nice.com.jzs.ui.doctors;
 
+import android.widget.TextView;
+
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
@@ -22,6 +24,11 @@ public class ActivityDoctorVisitTime extends AbstractActivity {
 
     @ViewById(R.id.container)
     ViewContainer viewContainer;
+    @ViewById(R.id.during_time)
+    TextView during_time;
+
+    @ViewById(R.id.tips)
+    TextView tips;
 
     @AfterViews
     void initView() {
@@ -35,7 +42,7 @@ public class ActivityDoctorVisitTime extends AbstractActivity {
             @Override
             public void loadSuccess(BaseBean bean) {
                 DoctorVisitTimeBean visitTimeBean = (DoctorVisitTimeBean) bean;
-                if (visitTimeBean!=null){
+                if (visitTimeBean != null) {
                     updateView(visitTimeBean);
                 }
             }
@@ -47,26 +54,29 @@ public class ActivityDoctorVisitTime extends AbstractActivity {
         }.post(true, RequestAPI.API_JZB_DOCTORS_VISIT_TIME, params, DoctorVisitTimeBean.class);
     }
 
-    private void updateView(DoctorVisitTimeBean visitTimeBean){
+    private void updateView(DoctorVisitTimeBean visitTimeBean) {
 
-        viewContainer.removeAllViews();
         viewContainer.columns = 5;
-        viewContainer.hasDivider = 0;
+        viewContainer.hasDivider = 1;
+        viewContainer.dividerWid = 2;
 
-        if (visitTimeBean.getData().getVisit_times()!=null){
-            for (DoctorVisitTimeBean.DataBean.VisitTimesBean itemBean:visitTimeBean.getData().getVisit_times()){
+        if (visitTimeBean.getData().getVisit_times() != null) {
+            for (DoctorVisitTimeBean.DataBean.VisitTimesBean itemBean : visitTimeBean.getData().getVisit_times()) {
                 ViewDoctorVisitTimeItem timeItem = new ViewDoctorVisitTimeItem(ActivityDoctorVisitTime.this);
                 int index = visitTimeBean.getData().getVisit_times().indexOf(itemBean);
                 int color = getResources().getColor(R.color.white);
-                if (index%2 == 0){
+                if (index % 2 == 0) {
                     color = getResources().getColor(R.color.white);
-                }else {
-                    color = getResources().getColor(R.color.text_999999);
+                } else {
+                    color = getResources().getColor(R.color.white_two);
                 }
-                timeItem.setData(color,itemBean.getIs_visit());
+                timeItem.setData(color, itemBean.getIs_visit());
                 viewContainer.addView(timeItem);
             }
         }
+
+        during_time.setText(visitTimeBean.getData().getDuring_time());
+        tips.setText(visitTimeBean.getData().getTips());
 
     }
 
